564033cfd28ac8d2ec8626c53387b1fafd3e81f5
Park YeonJin <66981351+phantom-black@users.noreply.github.com>
2024-09-30 15:31:46 +0900

    [Silver V] Title: 막대기, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -7,19 +7,21 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int X = Integer.parseInt(br.readLine());
-        int stick = 64;  // 초기 막대 길이
+        int stick = 64;
         int stickCount = 0;
 
         while (X > 0) {
             if (stick > X) {
-                stick /= 2; // 현재 막대 길이가 목표보다 크면 반으로 줄임
+                stick /= 2;
             } else {
-                X -= stick; // 막대를 사용하고 목표 길이에서 빼줌
-                stickCount++; // 막대 카운트 증가
+                X -= stick; // X에서 사용한 stick 길이만큼 빼주기
+                stickCount++;
             }
         }
 
-        bw.write(String.valueOf(stickCount)); // stickCount 출력
+        // int값을 String으로 변환해서 출력
+        bw.write(String.valueOf(stickCount));
+        // BufferedWriter는 사용한 뒤 flush() - 남은 값 모두 출력, close()로 닫아줘야 함
         bw.newLine();
         bw.flush();
         bw.close();
