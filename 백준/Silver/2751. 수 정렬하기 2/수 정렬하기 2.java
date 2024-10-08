7f2e388f87f2d4a16c536296c8f51695db33ce6f
Park YeonJin <66981351+phantom-black@users.noreply.github.com>
2024-10-08 16:54:04 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4944 ms, Memory: 203036 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int n : list) {
+            System.out.println(n);
+        }
+    }
+}
