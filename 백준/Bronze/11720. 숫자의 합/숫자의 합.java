c5086a6b1fd82ec32dff48a28b7814fd850ab747
Park YeonJin <66981351+phantom-black@users.noreply.github.com>
2024-12-09 21:35:50 +0900

    [Bronze IV] Title: 숫자의 합, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -3,17 +3,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        br.readLine();
         String str = br.readLine();
         int sum = 0;
-
-        for(int i = 0; i < str.length(); i++) {
-            sum += Integer.parseInt(Character.toString(str.charAt(i)));
+        for (byte value : str.getBytes()) {
+            sum += value - '0';
         }
-        bw.write(String.valueOf(sum));
-        bw.flush();
-        bw.close();
+        System.out.println(sum);
         br.close();
     }
 }
